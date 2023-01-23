0ab60d6a857256447890db9787cc1ef5efe790af
MOFIJUL HAQUE <mofijulhoque995@gmail.com>
2023-01-23 19:02:30 +0530

    Node JS

---
+++ index.js
@@ -0,0 +1,25 @@
+// const http = require("http");
+
+// http.createServer(()=>{})
+
+// File based module=============================================
+
+// const v = {
+//     addition: (a, b) => {
+//         console.log(a + b);
+//     },
+//     percentage: (a, b) => {
+//         console.log((a + b)*100);
+//     }
+// }
+
+// module.exports = v;
+
+
+// =============================================================
+
+
+
+// +++++++++++++++++++++++++++++++Build In Module++++++++++++++++
+const fs = require('fs');
+console.log(fs);
